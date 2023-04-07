cfde3e125cb4698a8127b34e3806244bee58eefb
rrarima <5708@holbertonstudents.com>
2023-04-07 13:08:01 +1000

    initial commit

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""API request for employee name and todos completed"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    user = requests.get(url + "/users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "/todos", params={"userId": sys.argv[1]}).json()
+
+    total_tasks = len(todos)
+    completed_tasks = sum(1 for todo in todos if todo["completed"])
+
+    print("Employee {} is done with tasks({}/{}):".format(
+            user.get("name"), completed_tasks, total_tasks))
+
+    for todo in todos:
+        if todo["completed"]:
+            print(f"\t {todo['title']}")
